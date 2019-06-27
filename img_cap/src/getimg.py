e09697ae3c3ede8546790b6fac2bd938b9d32657
Bowen Xie <bxie1@ualberta.ca>
2019-06-27 15:40:38 -0700

    updated with keyboard action

--- img_cap/src/getimg.py
+++ img_cap/src/getimg.py
@@ -6,11 +6,11 @@ import numpy as np
 import csv
 import sys
 import os
-import glob
+from std_msgs.msg import String
 
 
 
-def save_img(data, name, current_dir, csv_name):
+def save_img(data, name, current_dir, csv_name, angle):
 	print('image saved!')
 	filepath = current_dir + '/' + csv_name
 	np_arr = np.fromstring(data.data, np.uint8)
@@ -18,7 +18,7 @@ def save_img(data, name, current_dir, csv_name):
 	cv2.imwrite(name, arr)
 	with open(filepath, 'a') as f:
 		writer = csv.writer(f)
-		row = [str(rospy.get_time()), current_dir + '/' + name]
+		row = [str(rospy.get_time()), current_dir + '/' + name, angle]
 		writer.writerow(row) 
 
 
@@ -28,40 +28,42 @@ def callback(data):
 	# define some of the variables
 	global i
 	global t1
+	global angle
 	current_dir = os.getcwd()
 	filename = 'img_loc.csv'
 
-	if i == 0:	
-		save_img(data, 'img_0.jpeg', current_dir, filename)
+	if i == 0:
+		angle = 90	
+		save_img(data, 'img_0.jpeg', current_dir, filename, angle)
+		print(angle)
 		t1 = rospy.get_time()
 		i += 1		
 	elif rospy.get_time()-t1 >= 1:
 		nam = 'img_{}.jpeg'.format(i)
-		save_img(data, nam, current_dir, filename)
+		save_img(data, nam, current_dir, filename, angle)
 		t1 = rospy.get_time()
 		i += 1
 	else:
 		pass
 
-#	try:
-#		print('image received!')
-#		cv2_img
-#		cv2_img = bridge.imgmsg_to_cv2(data, 'bgr8')
-#	except CvBridgeError, e:
-#		print(e)
-#
-#	else:
-#		global i
-#		nam = 'img_{}.jpg'.format(i)
-#		cv2.imwrite(nam, cv2_img)
-#		rospy.sleep(1)	
-#		i += 1 
+
+
+def callback_key(data):
+	delta = 3
+	global angle
+	if data.data == 'L':
+		angle -= delta
+	elif data.data == 'R':
+	 	angle += delta
+
+
 
 def main():
 	rospy.init_node('image_listener')
 	image_topic = '/raspicam_node/image/compressed'
+	key_topic = '/keys'
 	rospy.Subscriber(image_topic, CompressedImage, callback, queue_size=1)
-	print('aaaaaaaaaa')
+	rospy.Subscriber(key_topic, String, callback_key, queue_size=1)
 	rospy.spin()
 	
 if __name__ == "__main__":

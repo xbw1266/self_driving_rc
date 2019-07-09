cbb96d9f1b20aa68932e1179eed3b471407b2931
Bowen Xie <bxie1@ualberta.ca>
2019-07-09 15:37:49 -0700

    added keyboard action traning mode

--- train_data/touch.py
+++ train_data/touch.py
@@ -2,19 +2,22 @@ from keras.models import load_model
 import cv2
 import numpy as np
 from data_preprocess import Img_process
+from keras.optimizers import Adam
 
 
 def predict(img):
-	model = load_model('model.h5')
-	model.compile(loss='mse', optimizer='adam')
+	model = load_model('model_wasd.h5')
 	img = Img_process(img)
 	img_r = img.resize(320,160)
 	img_h = img.hsv()
 	img_b = img.blur()
 	img_d = img.detect()
 	img_d = np.expand_dims(img_d, axis=0)
-	y1, y2 = np.array(model.predict(img_d), dtype=np.uint8)
-	print(y1, y2)
+	key_map = ['w', 'a', 'd'] 
+	m = model.predict(img_d)
+	key = key_map[np.argmx(m)]
+	print(key)
+
 
 
 path = '/home/bowen/data/'
